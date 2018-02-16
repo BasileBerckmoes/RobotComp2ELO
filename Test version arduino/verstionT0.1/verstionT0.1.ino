54b9015fb1f31e704a61d1ea1011a1340a16d2b8
Basile Berckmoes <basile.berckmoes@student.odisee.be>
2018-02-16 15:10:30 +0100

    testen
    Voorbeeld programma toegevoegd voor uitlezen van afstands sensoren

--- Test version arduino/verstionT0.1/verstionT0.1.ino
+++ Test version arduino/verstionT0.1/verstionT0.1.ino
@@ -1,9 +1,15 @@
+#include <NewPing.h>
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("Test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for(int i = 0; i < 10; i++){
+    Serial.println(i);
+  }
+  //Serial.println("LOOP");
 }
